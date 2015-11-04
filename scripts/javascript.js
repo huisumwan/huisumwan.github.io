b5dde5607753275284a57a1dd608b6bab53066cb
huisumwan <rallyeducation@RALLYs-MacBook-Pro.local>
2015-11-04 16:29:25 -0500

    nabber scroll effect

---
+++ scripts/javascript.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    $(".js_intro_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#top").offset().top 
+        }, 1000);
+    });
+    $(".js_persona_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#persona1").offset().top 
+        }, 1000);
+    });
+     $(".js_persona1_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#persona1").offset().top 
+        }, 1000);
+    });
+      $(".js_persona2_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#persona2").offset().top 
+        }, 1000);
+    });
+      $(".js_persona3_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#persona3").offset().top 
+        }, 1000);
+    });
+      $(".js_scenario_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#scenario1").offset().top 
+        }, 1000);
+    });
+      $(".js_scenario1_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#scenario1").offset().top 
+        }, 1000);
+    });
+      $(".js_scenario2_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#scenario2").offset().top 
+        }, 1000);
+    });
+       $(".js_scenario3_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#scenario3").offset().top 
+        }, 1000);
+    });
+       $(".js_storyboard_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#storyboard").offset().top 
+        }, 1000);
+    });
+       $(".js_wireframe_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#wireframe").offset().top 
+        }, 1000);
+    });
+       $(".js_gui_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#gui").offset().top 
+        }, 1000);
+    });
+       $(".js_biblio_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#bibliography").offset().top 
+        }, 1000);
+    });
+       $(".js_about_link").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#aboutus").offset().top 
+        }, 1000);
+    });
+});
